4f82974f8629aaf613558a605fd5466900ef3635
SommayaHaque <67379154+SommayaHaque@users.noreply.github.com>
2023-04-18 19:55:28 -0400

    Add files via upload

---
+++ scripts/interactive.js
@@ -0,0 +1,37 @@
+document.getElementById("topNav").style.height = "50px";
+document.querySelectorAll(".menuitems").forEach(j => {
+  j.style.marginTop="100px";
+  j.classList.add("hidden");
+});
+
+document.getElementById("closebtn").addEventListener("click", function(){
+  for (i = 0; i < document.getElementById("closebtn").children.length; i++){
+    document.getElementById("closebtn").children[i].classList.toggle("active");
+  }});
+
+function collapse() {
+  if (document.getElementById("topNav").style.height == "200px") {
+    document.getElementById("topNav").style.height = "50px";
+    document.querySelectorAll(".menuitems").forEach(j => {
+      j.classList.add("hidden");
+      j.style.opacity="0";
+      j.style.marginTop="100px";
+    });
+  } else if (document.getElementById("topNav").style.height == "50px") {
+    document.getElementById("topNav").style.height = "200px";
+    document.querySelectorAll(".menuitems").forEach(j => {
+      j.classList.remove("hidden");
+      j.style.opacity="1.0";
+      j.style.marginTop="0px";
+    });
+  }}
+
+  $(document).on("scroll", function() {
+    $(".tag").each(function() {
+      if ($(this).position().top < $(window).scrollTop() + $(window).height()) {
+        $(this).addClass("visible");
+      } else {
+        $(this).removeClass("visible");
+      }
+    });
+  });
